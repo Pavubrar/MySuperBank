028612b20dc3b0df608a2b24d9c902cbc2fc4607
Pavubrar <56096198+Pavubrar@users.noreply.github.com>
2020-06-26 15:45:52 -0600

    Add project files.

---
+++ BankAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MySuperBank
+{
+    public class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { get;  }
+
+        public BankAccount(string name, decimal initialAmount)
+        {
+            this.Owner = name;
+            this.Balance = initialAmount;
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+
+        }
+        public void MakeWithdraw(decimal amount, DateTime date, string note)
+        {
+
+        }
+    }
+}
